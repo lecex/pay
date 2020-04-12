df12047842ebac2e8f1d5a515025ab4624e7fc86
bigrocs <bigrocs@qq.com>
2020-04-12 16:12:05 +0800

    自动提交 git 代码

--- service/alipay.go
+++ service/alipay.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"fmt"
-
 	"github.com/lecex/core/env"
 	proto "github.com/lecex/pay/proto/pay"
 	"github.com/shopspring/decimal"
@@ -38,8 +36,8 @@ func (srv *Alipay) AopF2F(order *proto.Order) (ok bool, err error) {
 	body.Set("out_trade_no", order.OrderNo)
 	body.Set("total_amount", decimal.NewFromFloat(float64(order.TotalAmount)).Div(decimal.NewFromFloat(float64(100))))
 	body.Set("timeout_express", "2m")
-	body.Set("extend_params", map[string]interface{}{"sys_service_provider_id": env.Getenv("ALIPAY_SERVICE_PID", "2088002104076813")})
-	fmt.Println(body)
+	body.Set("extend_params", map[string]interface{}{"sys_service_provider_id": env.Getenv("ALIPAY_SERVICE_PID", "2088831013879013")})
+
 	aliRsp, err := srv.Client.TradePay(body)
 	if err != nil {
 		return ok, err
