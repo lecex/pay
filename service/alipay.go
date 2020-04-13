029627501678b63dfa66faab857f1dc2f52bdef3
bigrocs <bigrocs@qq.com>
2020-04-13 09:59:22 +0800

    增加第三方授权

--- service/alipay.go
+++ service/alipay.go
@@ -36,6 +36,7 @@ func (srv *Alipay) AopF2F(order *proto.Order) (ok bool, err error) {
 	body.Set("out_trade_no", order.OrderNo)
 	body.Set("total_amount", decimal.NewFromFloat(float64(order.TotalAmount)).Div(decimal.NewFromFloat(float64(100))))
 	body.Set("timeout_express", "2m")
+	body.Set("app_auth_token", "202004BB360a6ab6cc114445be8dd9d589483E67")
 	body.Set("extend_params", map[string]interface{}{"sys_service_provider_id": env.Getenv("ALIPAY_SERVICE_PID", "2088831013879013")})
 
 	aliRsp, err := srv.Client.TradePay(body)
