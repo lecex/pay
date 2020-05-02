e5dec0cdc2a9a589052c6d8184ade8e8b0ae5002
bigrocs <bigrocs@qq.com>
2020-05-02 18:39:01 +0800

    自动提交 git 代码

--- handler/pay.go
+++ handler/pay.go
@@ -175,18 +175,6 @@ func (srv *Pay) AopF2F(ctx context.Context, req *pd.Request, res *pd.Response) (
 			return nil
 		}
 		res.Content = string(c) //数据正常返回
-		log.Fatal("AopF2F.Alipay", req, res, err)
-		if content["code"].(string) == "10000" && content["msg"].(string) == "Success" {
-			log.Fatal("AopF2F.Alipay.Success", req, res, err)
-			res.Valid = true
-			err = srv.successOrder(config.Alipay.Fee)
-			if err != nil {
-				res.Error.Code = "AopF2F.Alipay.Update.Success"
-				res.Error.Detail = "支付成功,更新订单状态失败!"
-				log.Fatal("AopF2F.Alipay.successOrder", req, res, err)
-			}
-			return nil
-		}
 		return nil
 	case "wechat":
 		srv.newWechatClient(config) //实例化微信连接
@@ -205,18 +193,6 @@ func (srv *Pay) AopF2F(ctx context.Context, req *pd.Request, res *pd.Response) (
 			return nil
 		}
 		res.Content = string(c) //数据正常返回
-		log.Fatal("AopF2F.Wechat", req, res, err)
-		if content["result_code"] == "SUCCESS" {
-			log.Fatal("AopF2F.Wechat.Success", req, res, err)
-			res.Valid = true
-			err = srv.successOrder(config.Wechat.Fee)
-			if err != nil {
-				res.Error.Code = "AopF2F.Wechat.Update.Success"
-				res.Error.Detail = "支付成功,更新订单状态失败!"
-				log.Fatal("AopF2F.Wechat.successOrder", req, res, err)
-			}
-			return nil
-		}
 		return nil
 	}
 	return nil
